388974f5b68950cb1225051fd8ad0fbeb158f88f
Marian Radu <doradu@pentalog.fr>
2015-06-06 15:18:26 +0300

    -branch:master-

--- assets/js/drag.js
+++ assets/js/drag.js
@@ -1,3 +1,19 @@
-/**
- * Created by pellu on 6/6/2015.
- */
+var options = {enableGestures: true};
+
+Leap.loop(options, function (frame) {
+    if (frame.pointables.length > 0) {
+        var i = 1;
+        frame.pointables.forEach(function (pointable) {
+            var position = pointable.stabilizedTipPosition;
+            var normalized = frame.interactionBox.normalizePoint(position);
+            var x = window.innerWidth * normalized[0];
+            var y = window.innerHeight * (1 - normalized[1]);
+            $('.finger' + i).css({
+                left: x,
+                top: y
+            });
+            i++;
+        });
+
+    }
+});
