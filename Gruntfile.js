68e27b8a8d79b11327ed9a5b80a5d596a6ce94a0
Mr. Saturday <penumbra.scilicet@gmail.com>
2015-07-10 01:00:41 +0300

    [IMPROVE] Styles
    - ignored cache files
    - recreated CSS with SASS
    - moved index.html to index_old.html for reference
    
    Signed-off-by: Mr. Saturday <penumbra.scilicet@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,8 +27,8 @@ module.exports = function (grunt) {
         },
         watch: {
             scripts: {
-                files: ['assets/**/*.js', 'spec/**/*.js'],
-                tasks: ['jshint', 'jslint', 'jasmine'],
+                files: ['assets/**/*.js', 'spec/**/*.js', 'index.jade', 'src/**/*.scss'],
+                tasks: ['jade', 'sass', 'jshint', 'jslint', 'jasmine'],
                 options: {
                     spawn: false
                 }
@@ -41,11 +41,32 @@ module.exports = function (grunt) {
                     specs: 'spec/*Spec.js'
                 }
             }
+        },
+        jade: {
+            compile: {
+                files: {
+                    'index.html': 'index.jade'
+                }
+            }
+        },
+        sass: {
+            compile: {
+                files: {
+                    'dist/assets/css/main.css': 'src/client/main.scss'
+                },
+                options: {
+                    loadPath: ['node_modules/bootstrap-sass/assets/stylesheets'],
+                    style: 'compressed',
+                    compass: true
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-jslint');
 };
