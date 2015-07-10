5b8eea996ea9977404acb35e9643bf7735c38eb4
Mr. Saturday <penumbra.scilicet@gmail.com>
2015-07-10 23:05:38 +0300

    Socket client switch
    - changed socket client to SockJS
    
    Signed-off-by: Mr. Saturday <penumbra.scilicet@gmail.com>

--- spec/SocketProxySpec.js
+++
@@ -1,9 +0,0 @@
-define(['SocketProxy'], function (SocketProxy) {
-    describe('Socket Proxy', function() {
-        describe('should fail init', function() {
-            it('when invalid config is passed', function() {
-                expect(function () {SocketProxy.init({});}).toThrow(new Error('Invalid config state'));
-            });
-        });
-    });
-});
--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,10 @@
 require.config({
-    baseUrl: 'dist/assets/js'
+    baseUrl: 'dist/assets/js',
+    paths: {
+        'SockJS': '//cdn.jsdelivr.net/sockjs/1.0.0/sockjs.min'
+    }
 });
 
-require(['LayoutManager', 'SocketProxy']);
+require(['LayoutManager', 'SockJS'], function (LayoutManager, SockJS) {
+    var socket = new SockJS('http://localhost:9000/');
+});
